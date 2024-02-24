a607a4348e127e1d56cc4326c3a6a685001c8cd7
AhmedSaeed77 <ahmedsaeed1722@gmail.com>
2024-02-24 10:33:39 +0200

    cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'Lang' => \App\Http\Middleware\Language::class,
-        'cors' => \App\Http\Middleware\Core::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
