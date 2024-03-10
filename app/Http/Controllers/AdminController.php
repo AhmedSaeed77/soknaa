704830bd6530550e0cff5995540f986a7a6c40e2
AhmedSaeed77 <ahmedsaeed1722@gmail.com>
2024-03-11 01:01:13 +0200

    ss

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -48,8 +48,19 @@ class AdminController extends Controller
         $admins = Admin::when($request->name, function ($query) use ($request) {
             return $query->where('name', 'like', '%' . $request->name . '%');
         })
+        // ->when($request->role, function ($query) use ($request) {
+        //     return $query->where('role', 'like', '%' . $request->role . '%');
+        // })
         ->when($request->role, function ($query) use ($request) {
-            return $query->where('role', 'like', '%' . $request->role . '%');
+            $roleMapping = [
+                'admin' => ['super admin', 'admin'],
+                // Add more mappings if needed
+            ];
+        
+            $role = $request->role;
+            $mappedRoles = $roleMapping[$role] ?? [$role];
+        
+            return $query->whereIn('role', $mappedRoles);
         })
         ->when($request->date == 1, function ($query) {
             return $query->whereDate('created_at', now()->toDateString());
