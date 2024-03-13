1e4c738bd4b4188d5396951f217f7492b7576fac
AhmedSaeed77 <ahmedsaeed1722@gmail.com>
2024-03-14 00:20:11 +0200

    ss

--- app/Http/Resources/dashboard/FromMesageResource.php
+++ app/Http/Resources/dashboard/FromMesageResource.php
@@ -16,10 +16,10 @@ class FromMesageResource extends JsonResource
     {
         // return parent::toArray($request);
         return [
-                    'id' => $this->message['id'],
+                    'id' => $this->message['id'] ?? null,
                     'type' => $this->type,
-                    'date' => $this->message['created_at']->format('l, H:i'),
-                    'message' => $this->message['message'],
+                    'date' => $this->message['created_at']->format('l, H:i') ?? null,
+                    'message' => $this->message['message'] ?? null,
                 ];
     }
 }
