0f6733c6e094e4e3eb61020a3d895db15fd6e916
AhmedSaeed77 <ahmedsaeed1722@gmail.com>
2024-08-15 21:59:41 +0300

    chat

--- app/Models/User.php
+++ app/Models/User.php
@@ -135,20 +135,29 @@ class User extends Authenticatable implements JWTSubject
         $lastMembershipNumber = static::max('membership_num') ?? 999;
         return $lastMembershipNumber + 1;
     }
-    
+
     public function chats()
     {
         return $this->hasMany(User::class, 'from_user');
     }
-    
+
     public function privateChats()
     {
         return $this->hasMany(PrivatChat::class, 'from_user');
     }
-    
+
      public function latestPrivateChat()
     {
         return $this->privateChats()->latest('created_at')->first();
+    }
+    public function chatRooms()
+    {
+        return $this->hasMany(ChatRoomMember::class, 'user_id');
+    }
+
+    public function chatRoomMessage()
+    {
+        return $this->hasMany(ChatRoomMessage::class, 'user_id');
     }
 
 }
